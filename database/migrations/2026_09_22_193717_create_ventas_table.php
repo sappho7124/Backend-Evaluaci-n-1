22e75fd179f6ab5a630c40bbb9bd3b8150a0d5e0
MAHQ729 <miguelito.04032007@gmail.com>
2026-09-22 14:39:43 -0500

    Migration's creation(Categorias, Productos, ventas)

---
+++ database/migrations/2026_09_22_193717_create_ventas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nombre_cliente', 150);
+            $table->string('correo_cliente', 150);
+            $table->string('numero_tarjeta', 20);
+
+            $table->json('productos');
+
+            $table->decimal('valor_total', 10, 2);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
